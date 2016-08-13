c633bffe718ec33fcc7155678617c8ed36ce02a0
Gregory Schwedock <schwedock@gmail.com>
2016-08-13 18:33:51 -0400

    Basic showing list titles and content from repeater.

--- list-template.php
+++ list-template.php
@@ -1,10 +1,38 @@
 <?php
 /**
- * Template Name: Custom Template
+ * Template Name: List Template
  */
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
+  HEY HO o
+
   <?php get_template_part('templates/content', 'page'); ?>
+
+<?php
+
+// check if the repeater field has rows of data
+if( have_rows('list') ):
+
+  // loop through the rows of data
+    while ( have_rows('list') ) : the_row();
+
+        // display a sub field value
+?>
+  <h2><?php the_sub_field('name'); ?></h2>
+  <?php the_sub_field('top_content'); ?>
+<?php
+
+    endwhile;
+
+else :
+
+    // no rows found
+
+endif;
+
+?>
+
+
 <?php endwhile; ?>
