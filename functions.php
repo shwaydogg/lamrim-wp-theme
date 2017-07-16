1dc53fa13b9f96178b1547d2d74e599dd5c2231e
Greg <schwedock@gmail.com>
2017-07-16 23:19:21 +0000

    fixed active nav class and overflow text on sidebar

--- functions.php
+++ functions.php
@@ -26,3 +26,12 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+    if (in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+    }
+    return $classes;
+}
