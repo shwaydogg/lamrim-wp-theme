299c5460bb140ea2a4be82a6e3350662549bedc1
Greg <schwedock@gmail.com>
2017-11-25 20:12:46 +0000

    updated header fonts

--- templates/head.php
+++ templates/head.php
@@ -3,5 +3,5 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One|Patua+One|Material+Icons" rel="stylesheet">  
 </head>
