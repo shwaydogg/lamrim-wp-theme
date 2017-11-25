86720a90e129730d44e4f498817bcd3c1aab13af
Greg <schwedock@gmail.com>
2017-11-25 19:25:54 +0000

    Endless knot as link icon

--- list-template.php
+++ list-template.php
@@ -21,19 +21,19 @@
           <img class="list-item-img" src="<?=get_sub_field("super_level_image")?>">
           <h2>
             <div class="container">
-              <a class="copy-link" href="#list-item-<?="$i"?>-super"><i class="link material-icons">link</i><?php the_sub_field('super_level'); ?></a>
+              <a class="copy-link" href="#list-item-<?="$i"?>-super"><i class="link material-icons endless-knot"></i><?php the_sub_field('super_level'); ?></a>
             </div>
           </h2>
         </li>        
         <?php } ?>
         <li id="list-item-<?="$i"?>" class="list-item container"  name="<?="$i"?>">
-          <h2><a class="copy-link" href="#list-item-<?="$i"?>"><i class="link material-icons">link</i><?php the_sub_field('title'); ?></a></h2>
+          <h2><a class="copy-link" href="#list-item-<?="$i"?>"><i class="link material-icons endless-knot"></i><?php the_sub_field('title'); ?></a></h2>
           <hr/>
           <?php the_sub_field('top_content'); ?>
           <ul>
             <?php if( have_rows('sub_list') ): while ( have_rows('sub_list') ) : the_row(); $j = get_sub_field("uid");?>
               <li id="list-item-<?="$i-$j"?>" class="list-item sub" name="<?="$i-$j"?>">
-              <h3><a class="copy-link" href="#list-item-<?="$i-$j"?>"><i class="link material-icons">link</i><?php the_sub_field('title'); ?></a></h3>
+              <h3><a class="copy-link" href="#list-item-<?="$i-$j"?>"><i class="link material-icons endless-knot"></i><?php the_sub_field('title'); ?></a></h3>
               <?php the_sub_field('content'); ?>
               </li>
             <?php endwhile; endif;?>
